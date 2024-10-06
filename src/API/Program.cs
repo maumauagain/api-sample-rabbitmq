fc2f7b9ccb3d21d256db8a05e6e9d889807a8895
maumauagain <amauri.martins.jr@hotmail.com>
2024-10-06 01:45:20 -0300

    adding virtual host and reverse proxy

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -13,31 +13,32 @@ builder.Services.AddSwaggerGen();
 var rabbitMQUrl = builder.Configuration?.GetValue<string>("RabbitMQ:Url");
 var rabbitMQUsername = builder.Configuration?.GetValue<string>("RabbitMQ:Username");
 var rabbitMQPassword = builder.Configuration?.GetValue<string>("RabbitMQ:Password");
+var rabbitMQVHost = builder.Configuration?.GetValue<string>("RabbitMQ:VHost");
 
 builder.Services.AddMassTransit(x =>
 {
-    x.AddConsumer<ProcessedReportEventConsumer>();
-    x.UsingRabbitMq((ctx, cfg) =>
-    {
-        cfg.Host(rabbitMQUrl, host =>
-        {
-            host.Username(rabbitMQUsername!);
-            host.Password(rabbitMQPassword!);
-        });
-        
-        cfg.ReceiveEndpoint("minha-fila", e =>
-        {
-            e.ConfigureConsumeTopology = false;
-            e.ConfigureConsumer<ProcessedReportEventConsumer>(ctx);
-            e.Bind("minha-exchange", x =>
-            {
-                x.ExchangeType = "direct";
-                x.RoutingKey = "minha-rota";
-            });
-        });
+	x.AddConsumer<ProcessedReportEventConsumer>();
+	x.UsingRabbitMq((ctx, cfg) =>
+	{
+		cfg.Host(rabbitMQUrl, rabbitMQVHost,  host =>
+		{
+			host.Username(rabbitMQUsername!);
+			host.Password(rabbitMQPassword!);
+		});
+		
+		cfg.ReceiveEndpoint("minha-fila", e =>
+		{
+			e.ConfigureConsumeTopology = false;
+			e.ConfigureConsumer<ProcessedReportEventConsumer>(ctx);
+			e.Bind("minha-exchange", x =>
+			{
+				x.ExchangeType = "direct";
+				x.RoutingKey = "minha-rota";
+			});
+		});
 
-        cfg.ConfigureEndpoints(ctx);
-    });
+		cfg.ConfigureEndpoints(ctx);
+	});
 });
 
 var app = builder.Build();
@@ -45,21 +46,21 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.MapPost("/reports", async (string name, IPublishEndpoint bus, ISendEndpointProvider endpoint) =>
 {
-    var queue = await endpoint.GetSendEndpoint(new Uri("queue:minha-fila")); 
-    var report = new Report() { Name = name};
-    ReportList.Reports.Add(report);
-    var msg = new ProcessedReportEvent(report.Id);
-    //await bus.Publish(msg);
-    await queue.Send<ProcessedReportEvent>(msg);
-    return Results.Ok(report);
+	var queue = await endpoint.GetSendEndpoint(new Uri("queue:minha-fila")); 
+	var report = new Report() { Name = name};
+	ReportList.Reports.Add(report);
+	var msg = new ProcessedReportEvent(report.Id);
+	//await bus.Publish(msg);
+	await queue.Send<ProcessedReportEvent>(msg);
+	return Results.Ok(report);
 });
 
 app.MapGet("/reports", () => ReportList.Reports.ToArray());
